9fe48af39a3caf3bc2bc96062db27217c56f0f6e
xivan74 <ivan.khruchshev@gmail.com>
2019-07-20 16:22:22 +0500

    Добавлена проверка формы и URL страницы логина

---
+++ pages/locators.py
@@ -0,0 +1,10 @@
+from selenium.webdriver.common.by import By
+
+class MainPageLocators(object):
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+class LoginPageLocators(object):
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+    LOGIN_URL = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+
